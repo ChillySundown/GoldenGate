7ee6718a93337fef3469bd531dea8152bdd6bb38
TadehMouradian <135496543+TadehMouradian@users.noreply.github.com>
2024-02-05 11:05:13 -0800

    Add files via upload

---
+++ DataBase.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+public class DataBase {
+    private Map<Integer, String> database;
+
+    public DataBase(String names, String ids)
+    {
+        database = new HashMap<Integer, String>();
+        setDataBase(names, ids);
+    }
+
+    private void setDataBase(String names, String ids)
+    {
+        Scanner in1 = new Scanner(new File(names));
+        Scanner in2 = new Scanner(new File(ids));
+
+        while(in1.hasNext() && in2.hasNext())
+        {
+            int id = in2.nextInt();
+            String name = in1.next();
+
+            database.put(id, name);
+        }
+    }
+
+    private boolean validId(int id)
+    {
+        return database.containsKey(id);
+    }
+
+    public String getName(int id)
+    {
+        if(validId(id))
+            return database.get(id);
+        else
+            return "Invalid";
+    }
+}
