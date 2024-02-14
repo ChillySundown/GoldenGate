e205611c99a3938c8dcef535ffbcf1bc2f732d03
TadehMouradian <135496543+TadehMouradian@users.noreply.github.com>
2024-02-14 19:38:03 +0000

    Commit TestScanner main -> class (Testing required)

--- TestScanner.java
+++ TestScanner.java
@@ -1,67 +1,75 @@
 import java.util.Scanner;
 import java.time.*;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 public class TestScanner
 {
-    public static void main(String[] args)
+    Scanner input;
+    DataBase d;
+    boolean use;
+    int idUse;
+    LocalTime checkIn;
+    LocalTime checkOut;
+    String msg;
+    boolean contin;
+
+    public TestScanner()
     {
-        Scanner input = new Scanner(System.in);
-        DataBase d = new DataBase("names.txt", "ids.txt");
-        boolean use = false;
-        int idUse = -1;
-        int inp;
-        LocalTime checkIn = LocalTime.now();
-        LocalTime checkOut = LocalTime.now();
-        String msg = "";
-        boolean contin = true;
-        try
+        input = new Scanner(System.in);
+        d = new DataBase("names.txt", "ids.txt");
+        use = false;
+        idUse = -1;
+        checkIn = LocalTime.now();
+        checkOut = LocalTime.now();
+        msg = "";
+        contin = true;
+    }
+
+    public String scan(int id)
+    {
+        if(use)
         {
-            while(contin)
+            if(id == idUse)
             {
-                inp = input.nextInt();
-                if(inp == -1)
-                {
-                    contin = false;
-                }
-                else if(use)
-                {
-                    if(inp == idUse)
-                    {
-                        use = false;
-                        checkIn = LocalTime.now();
-                        msg = d.getName(inp) + " checked in the hallpass at " + checkIn + ". (" + Duration.between(checkOut, checkIn) + ")\n";
-                        Files.write(Paths.get("log.txt"), msg.getBytes(), StandardOpenOption.APPEND);
-                    }
-                    else
-                    {
-                        System.out.println("Someone else is using the hallpass.");
-                    }
-                }
-                else
-                {
-                    if(d.validId(inp))
-                    {   
-                        use = true;
-                        idUse = inp;
-                        checkOut = LocalTime.now();
-                        msg = d.getName(inp) + " checked out the hallpass at " + checkOut + ".\n";
-                        Files.write(Paths.get("log.txt"), msg.getBytes(), StandardOpenOption.APPEND);
-                    }
-                    else
-                    {
-                        System.out.println("Invalid Id");
-                    }
-                }
+                use = false;
+                checkIn = LocalTime.now();
+                msg = d.getName(inp) + " checked in the hallpass at " + checkIn + ". (" + Duration.between(checkOut, checkIn) + ")\n";
+                return msg;
+            }
+            else
+            {
+                return "Someone else is using the hallpass.";
             }
         }
-        catch(IOException e)
+        else
         {
-            System.out.println("ERROR");
+            if(d.validId(id))
+            {   
+                use = true;
+                idUse = id;
+                checkOut = LocalTime.now();
+                msg = d.getName(inp) + " checked out the hallpass at " + checkOut + ".\n";
+                return msg;
+            }
+            else
+            {
+                return "Invalid Id";
+            }
         }
-        System.out.println("END");
-        input.close();
+    }
+    public String curOut()
+    {
+        if(use)
+        {
+            return d.getName(idUse);
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    public boolean validId(int id)
+    {
+        return d.validId(id);
     }
 }
