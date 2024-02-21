c3c9a4f5a9ccb130befe49269cf03a80c906514b
TadehMouradian <135496543+TadehMouradian@users.noreply.github.com>
2024-02-21 18:51:45 +0000

    Commit Back End Done For Now

--- TempTesting.java
+++ TempTesting.java
@@ -5,20 +5,20 @@ public class TempTesting
     {
         try
         {
-        TestScanner t = new TestScanner();
-        System.out.println(t.scan(960031902));
-        TimeUnit.SECONDS.sleep(10);
-        System.out.println(t.scan(960031902));
-        System.out.println(t.scan(96003190));
-        System.out.println(t.scan(960031902));
-        System.out.println(t.scan(96003190));
-        System.out.println(t.scan(960031902));
-        System.out.println(t.validId(960031902));
-        System.out.println(t.validId(96003190));
-        System.out.println(t.scan(960031902));
-        System.out.println(t.curOut());
-        System.out.println(t.scan(960031902));
-        System.out.println(t.curOut());
+            TestScanner t = new TestScanner();
+            System.out.println(t.scan(960031902));
+            TimeUnit.SECONDS.sleep(101);
+            System.out.println(t.scan(960031902));
+            System.out.println(t.scan(96003190));
+            System.out.println(t.scan(960031902));
+            System.out.println(t.scan(96003190));
+            System.out.println(t.scan(960031902));
+            System.out.println(t.validId(960031902));
+            System.out.println(t.validId(96003190));
+            System.out.println(t.scan(960031902));
+            System.out.println(t.curOut());
+            System.out.println(t.scan(960031902));
+            System.out.println(t.curOut());
         }
         catch(Exception e)
         {
