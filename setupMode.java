97acfc35482a52dda6a4de2409054c0c19427b10
Abbas Darwich <fanamatic2.0@gmail.com>
2024-03-25 11:43:14 -0700

    Almost heaven...

--- setupMode.java
+++ setupMode.java
@@ -11,11 +11,36 @@ public class setupMode {
         final JLabel greeting = new JLabel("Welcome to Setup Mode!");
         final JLabel info = new JLabel("Please enter an admin password");
         final JPasswordField adminField = new JPasswordField();
+        final JLabel result = new JLabel("");
         greeting.setBounds(120, 250, 200, 50);
         info.setBounds(120, 350, 200, 30);
         adminField.setBounds(120, 400, 200, 30);
         miniScreen.add(greeting);
         miniScreen.add(info);
         miniScreen.add(adminField);
+        adminField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent code) {
+                String password = new String(adminField.getPassword());
+                try {
+                    int val = Integer.parseInt(password);
+                    for(int i : adminField)
+                    {
+                        if(i == val) {
+                            //Add setUp here
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        });
+        miniScreen.setSize(480,400);
+        miniScreen.setLayout(null);
+        miniScreen.setVisible(true);
+        miniScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args)
+    {
+        new setupMode();
     }
 }
