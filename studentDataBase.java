f843f53957e0b9fd9c4e816445133db30699162b
Abbas Darwich <fanamatic2.0@gmail.com>
2024-03-28 11:42:58 -0700

    jhgfjhj

--- studentDataBase.java
+++ studentDataBase.java
@@ -8,13 +8,15 @@ public class studentDataBase {
         JFrame screen = new JFrame("Student Database - GoldenGate");
         JLabel welcome = new JLabel("Welcome to the Student DataBase! You can add or remove kids from the hallpass registry");
         JTable kids = new JTable(15, 2);
-        welcome.setBounds(350, 300, 200, 50);
-        welcome.setFont(new Font("Comic Sans", Font.PLAIN, 23));
+        welcome.setBounds(10, 300, 600, 50);
+        welcome.setFont(new Font("Comic Sans", Font.PLAIN, 16));
+        kids.setBounds(200, 500, 30, 40);
         screen.setBackground(Color.DARK_GRAY);
         screen.setSize(880, 800);
         screen.add(welcome);
         screen.add(kids);
         screen.setVisible(true);
+        screen.setLayout(null);
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
